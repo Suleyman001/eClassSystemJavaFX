16420416fa6bb1f2ea31a181bd1732835609f5cc
Suleyman Jumaniyazov <suleyman290401@gmail.com>
2024-12-01 18:13:41 +0100

    Fully working JAR and Database and dependencies, bug fixes and DOWNLOAD.zip is fully working

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,48 +1,32 @@
 module com.example.eclasssystem {
-    // JavaFX Modules
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.graphics;
-
-    // Logging Modules
-    requires org.apache.logging.log4j;
-    requires org.slf4j;
-
-    // Database
     requires java.sql;
+
+    // Simplified SLF4J requirements
+    requires org.slf4j;
+    requires static java.logging;
+
     requires org.xerial.sqlitejdbc;
-
-    // HTTP and Network
-    requires okhttp3;
-    requires org.apache.httpcomponents.httpcore;
-    requires org.apache.httpcomponents.httpclient;
-    requires java.net.http;
-
-    // File Processing
-    requires org.apache.poi.poi;
-    requires org.apache.poi.ooxml;
     requires com.opencsv;
+    requires org.apache.poi.ooxml;
     requires itextpdf;
 
-    // XML Processing
+
+    requires java.net.http;
     requires org.jsoup;
-    requires xerces;
 
-    // Reflection and Runtime
-    requires java.desktop;
-
-    // Export and Open Packages
+    // Export and open necessary packages
     exports com.example.eclasssystem;
     exports com.example.eclasssystem.controller;
     exports com.example.eclasssystem.services;
     exports com.example.eclasssystem.util;
     exports com.example.eclasssystem.exceptions;
-    exports com.example.eclasssystem.mnbdownloader;
 
-    // Open packages for reflection and JavaFX
     opens com.example.eclasssystem to javafx.fxml;
     opens com.example.eclasssystem.controller to javafx.fxml;
     opens com.example.eclasssystem.services to javafx.base;
+
+
     opens com.example.eclasssystem.mnbdownloader to javafx.base;
-    opens com.example.eclasssystem.util to javafx.base;
 }
